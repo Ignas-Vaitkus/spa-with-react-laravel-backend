84e6ad4ffc9716f89203439268489d6ed4e0975b
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-11-16 17:58:31 +0200

    Added get routes

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,58 @@
 <?php
 
+use App\Models\Employee;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get(
+    '/projects',
+    function (Request $request) {
+        return response()->json(
+            Project::all(),
+            200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get(
+    '/project-employees/{id}',
+    function (Request $request, $id) {
+        $project = Project::find($id);
+
+        return response()->json(
+            $project->employees,
+            200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+);
+
+Route::get(
+    '/employees',
+    function (Request $request) {
+        return response()->json(
+            Employee::all(),
+            200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+);
+
+Route::get(
+    '/employee-projects/{id}',
+    function (Request $request, $id) {
+        $employee = Employee::find($id);
+
+        return response()->json(
+            $employee->projects,
+            200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+);
